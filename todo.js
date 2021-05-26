87851fd632c01f6632ec4602673a3e1e13ab218b
LidoKing <kinglido203@gmail.com>
2021-05-26 17:55:56 +0800

    shows todo tasks after added

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
 const input = document.getElementById('input');
 const button = document.getElementById('button');
+const todos = document.getElementById('todos');
+const body = document.getElementsByTagName('body');
 
 let tasks = [];
 
@@ -12,7 +14,14 @@ input.addEventListener("keypress", event => {
 
 button.addEventListener("click", () => {
   if(input.value != "") {
+    // Back-end oprations
     tasks.push(input.value);
+    localStorage.setItem("savedTasks", tasks);
+
+    // Front-end update
+    let task = `<li>${input.value}</li>`;
+    todos.innerHTML += task;
+
     input.value = "";
   } else {
     alert("You dont't have any tasks to add!");
