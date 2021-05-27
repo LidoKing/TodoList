18dc2c795895a3da72ca20a83b81154864040d70
LidoKing <kinglido203@gmail.com>
2021-05-27 22:01:22 +0800

    added button to clear all tasks

--- todo.js
+++ todo.js
@@ -1,25 +1,27 @@
 const input = document.getElementById('input');
-const button = document.getElementById('button');
+const addBtn = document.getElementById('addBtn');
 const todos = document.getElementById('todos');
-const body = document.getElementsByTagName('body');
+const clearBtn = document.getElementById('clearBtn');
+const task = document.getElementsByTagName('LI');
 
 let tasks = [];
 
 input.addEventListener("keypress", event => {
   if (event.keyCode == 13) {
    event.preventDefault();
-   button.click();
+   addBtn.click();
   }
 });
 
-button.addEventListener("click", () => {
+addBtn.addEventListener("click", () => {
   if(input.value != "") {
     // Back-end oprations
     tasks.push(input.value);
+    console.log(tasks);
     localStorage.setItem("savedTasks", JSON.stringify(tasks)); // localStorage only supports strings
 
     // Front-end update
-    todos.innerHTML += `<li>${input.value}</li>`;
+    todos.innerHTML += `<li onclick="crossout(this)">${input.value}</li>`; // Use 'this' to get the li element that called function
 
     input.value = "";
   } else {
@@ -27,9 +29,29 @@ button.addEventListener("click", () => {
   }
 });
 
+clearBtn.addEventListener("click", () => {
+  localStorage.removeItem("savedTasks");
+  todos.innerHTML = "";
+  tasks = [];
+});
+
+function crossout(elem) {
+  let indexOfTask = tasks.indexOf(elem.textContent);
+  if(task[indexOfTask].classList.contains("crossedOut")) {
+    task[indexOfTask].classList.remove("crossedOut");
+  } else {
+    task[indexOfTask].classList.add("crossedOut");
+  }
+}
+
 window.onload = () => {
-  let savedTasks = JSON.parse(localStorage.getItem("savedTasks")); // convert string back to array
-  savedTasks.forEach(task => {
-    todos.innerHTML += `<li>${task}</li>`;
-  });
+  if(localStorage.getItem("savedTasks") != null) {
+    tasks = JSON.parse(localStorage.getItem("savedTasks"));
+    let savedTasks = JSON.parse(localStorage.getItem("savedTasks")); // convert string back to array
+    savedTasks.forEach(item => {
+      todos.innerHTML += `<li onclick="crossout(this)">${item}<span class="removeTask">x</span></li>`;
+    });
+  } else {
+    localStorage.setItem("savedTasks", JSON.stringify(tasks));
+  }
 }
